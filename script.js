83927992463ae5370799b9b82c9a816a378cb39e
Anton B <43448390+KhanBakuX@users.noreply.github.com>
2023-09-29 23:58:38 +0300

    add main fucn to play - game fucn
    Added game func to play 5 rounds of the game and choose the winner

--- script.js
+++ script.js
@@ -40,44 +40,30 @@ const playRound = () => {
   }
 };
 
-playRound();
+const game = () => {
+  console.log("The game started! You have 5 rounds to win a computer");
 
-/* 
-Understading of a problem
+  let playerScore = 0;
+  let computerScore = 0;
 
-I need to write a programm which will play rock-paper-scissors with a user.
-User starts a game and the round of a game starts.  User chooses rock-paper-scissors. After the action is taken computer repsonds with
-rock-paper-scissors. After that comp evaluates the result and shows it to user. The evaluation and win is when paper beats rock, rock beats scissors, scissor beats 
-paper. In case of equality it is tie. Game plays 5 rounds. Comp counts the resulst for each player: win - 1 point in other cases it is 0.
-after 5 rounds comp shows the message who is winner and give a choice to a player if he wants to exit the game or play again.
+  for (let i = 0; i < 5; i++) {
+    let roundWinner = playRound();
+    if (roundWinner === "computer") ++computerScore;
+    if (roundWinner === "player") ++playerScore;
+  }
 
+  if (computerScore > playerScore)
+    console.log(
+      `You have lost the game! Your score is ${playerScore} and computer score is ${computerScore}`
+    );
+  else if (computerScore < playerScore)
+    console.log(
+      `You have won the game! Your score is ${playerScore} and computer score is ${computerScore}`
+    );
+  else
+    console.log(
+      `It's a tie! Your score is ${playerScore} and computer score is ${computerScore}`
+    );
+};
 
-Plan
-User interface is prompt
-
-Input:
-1. User starts a game in prompt
-2. User choose rock paper or scissors in each round in prompt
-3. User Chooses if he wants to a new 5 round or exit a program
-
-Output:
-1. the message who won a round
-2. the message who won a game
-
-Pseudocode
-
-Comp asks if user wants to start a game
-    IF he does not stop the program
-    If he wants start a game with 5 rounds in it
-    Initialize compScore and userScore
-    Loop from 1 to 5 
-        start a new round
-        decalre a winner of a round
-        message who won a round
-        add up score to wiiner in case of a win
-    Decalre a winner of a game
-    Ask user if wants to continue
-    If he wants start a ew game
-    if he doest end up a game.
-
-*/
+game();
