ac427bfe1c1286d9fbe163fbba5f55d1caa3de38
Anton B <43448390+KhanBakuX@users.noreply.github.com>
2023-10-06 19:10:16 +0300

    add UI to rock paper scissors, change code of it
    Added simple UI to the game and rewrited to bind it with UI
    using event listeners

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const getComputerChoice = () => {
   else return "scissors";
 };
 
-const getPlayerChoice = () => {
+/*const getPlayerChoice = () => {
   while (true) {
     let playerChoice = prompt("Enter your choice").toLowerCase();
     if (
@@ -16,31 +16,54 @@ const getPlayerChoice = () => {
       return playerChoice;
     console.log("your input is not rock or paper or scissors");
   }
-};
+};*/
 
 const playRound = () => {
-  const playerChoice = getPlayerChoice();
+  const playerChoice = event.target.textContent;
   const computerChoice = getComputerChoice();
+  let resultMessageString = "";
   if (playerChoice === computerChoice) {
-    console.log(
-      `It's tie! Your choice is ${playerChoice} and computer choice is ${computerChoice}`
-    );
-    return "tie";
+    resultMessageString = `It's tie! Your choice is ${playerChoice} and computer choice is ${computerChoice}`;
+    playerScore++;
+    computerScore++;
+    playerGameScore.textContent = `Player score: ${playerScore}`;
+    computerGameScore.textContent = `Computer score: ${computerScore}`;
+    resultMessage.textContent = resultMessageString;
   }
   if (
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
-    console.log(`You win! ${playerChoice} beats ${computerChoice}`);
-    return "player";
+    resultMessageString = `You win! ${playerChoice} beats ${computerChoice}`;
+    resultMessage.textContent = resultMessageString;
+    playerScore++;
+    playerGameScore.textContent = `Player score: ${playerScore}`;
+    computerGameScore.textContent = `Computer score: ${computerScore}`;
   } else {
-    console.log(`You lose!  ${computerChoice} beats ${playerChoice}`);
-    return "computer";
+    resultMessageString = `You lose!  ${computerChoice} beats ${playerChoice}`;
+    resultMessage.textContent = resultMessageString;
+    computerScore++;
+    playerGameScore.textContent = `Player score: ${playerScore}`;
+    computerGameScore.textContent = `Computer score: ${computerScore}`;
   }
+  if (playerScore === 5 || computerScore === 5) checkWinner();
 };
 
-const game = () => {
+function checkWinner() {
+  if (playerScore === computerScore)
+    resultMessage.textContent = "The game ended up with a tie";
+  else if (playerScore > computerScore)
+    resultMessage.textContent =
+      "Player wins the game, press any button to start a new game";
+  else
+    resultMessage.textContent =
+      "Computer wins the game, press any button to start a new game";
+  playerScore = 0;
+  computerScore = 0;
+}
+
+/*const game = () => {
   console.log("The game started! You have 5 rounds to win a computer");
 
   let playerScore = 0;
@@ -67,3 +90,12 @@ const game = () => {
 };
 
 game();
+*/
+let playerScore = 0;
+let computerScore = 0;
+const playerGameScore = document.querySelector("#player-score");
+const computerGameScore = document.querySelector("#computer-score");
+const resultMessage = document.querySelector("#result-message");
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", playRound));
